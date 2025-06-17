ac44b5f6ccdc097bead06400c62a323b82e3282d
DeclanTanner <116156795+Winnowmaker@users.noreply.github.com>
2025-06-17 16:19:14 +1000

    adding walls

--- src/main/java/dungeon/engine/GameEngine.java
+++ src/main/java/dungeon/engine/GameEngine.java
@@ -19,6 +19,22 @@ public class GameEngine {
         //Set the starting and ending cells
         map[0][0].setEntityType(EntityType.ENTRY);
         map[size-1][size-1].setEntityType(EntityType.EXIT);
+
+        createMazeWalls();
+    }
+
+    private void createMazeWalls()
+    {
+        int[][] wallCoordinates = new int[][] {
+                {0, 1}, {1, 1}, {2, 1}, {3, 1}, {4, 1}, {5, 1}, {6, 1},
+                {4, 4}, {4, 5}, {4, 6},
+                {7, 2}, {8, 2}, {9, 2}
+        };
+
+        for (int[] coordinate : wallCoordinates)
+        {
+            map[coordinate[0]][coordinate[1]].setEntityType(EntityType.WALL);
+        }
     }
 
     public Cell[][] getMap()
