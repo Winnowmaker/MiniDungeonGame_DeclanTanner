900f278e55b2741a5629648ffa0a558bd69f013c
DeclanTanner <116156795+Winnowmaker@users.noreply.github.com>
2025-06-18 12:28:36 +1000

    adding walls

--- src/main/java/dungeon/engine/GameEngine.java
+++ src/main/java/dungeon/engine/GameEngine.java
@@ -26,9 +26,11 @@ public class GameEngine {
     private void createMazeWalls()
     {
         int[][] wallCoordinates = new int[][] {
-                {0, 1}, {1, 1}, {2, 1}, {3, 1}, {4, 1}, {5, 1}, {6, 1},
-                {4, 4}, {4, 5}, {4, 6},
-                {7, 2}, {8, 2}, {9, 2}
+                {0, 1}, {1, 1}, {2, 1}, {2, 2}, {2, 3}, {1, 3}, {2, 5}, {2, 6},
+                {4, 3}, {4, 4}, {4, 5}, {4, 6}, {3, 5}, {1, 5}, {0, 7}, {0, 8},
+                {4, 1}, {5, 1}, {6, 1}, {6, 0}, {8, 1}, {8, 2}, {9, 2}, {8, 3},
+                {2, 8}, {2, 7}, {4, 8}, {5, 8}, {6, 8}, {6, 9}, {6, 7}, {6, 6},
+                {6, 3}, {6, 4}, {5, 4}, {8, 5}, {8, 6}, {7, 6}, {8, 8}, {9, 8},
         };
 
         for (int[] coordinate : wallCoordinates)
@@ -46,7 +48,8 @@ public class GameEngine {
         return map.length;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         GameEngine engine = new GameEngine(10);
         TextUI ui = new TextUI(engine);
         ui.start();
